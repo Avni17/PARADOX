931be5a8aafe8dfadeb73ae243fbce688c3d2a25
Avni17 <avni.arora2002@gmail.com>
2021-12-01 19:07:50 +0530

    signup

---
+++ paradox/signup.php
@@ -0,0 +1,47 @@
+<?php
+include('database.php');
+header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
+    header("Content-type:application/json");
+$encodedData = file_get_contents('php://input'); 
+$decodedData = json_decode($encodedData, true);
+$UserEmail = $decodedData['email'];
+$UserPW = ($decodedData['password']);
+$UserFullName = ($decodedData['fulln']);
+$UserGender = ($decodedData['gender']);
+$Username = ($decodedData['username']);
+//$radioVal = $_POST["gender];
+
+$SQL = "SELECT username FROM employee WHERE email = '$UserEmail'";
+
+$exeSQL = mysqli_query($con, $SQL);
+$checkEmail =  mysqli_num_rows($exeSQL);
+echo "$checkEmail";
+
+if ($checkEmail == 0) {
+  
+        
+              //  $result = mysqli_query($con,"select count(*) FROM employee")  or die( mysqli_error($con));
+               // $row = mysqli_num_rows($result);
+               // $c = $row+2;
+               // echo "$c";
+                $SQL = "INSERT INTO employee (name, username, password, email, gender) VALUES('$UserFullName','$Username','$UserPW','$UserEmail','$UserGender')";
+                $exeSQL = mysqli_query($con, $SQL);
+                if(mysqli_query($con, $SQL)){
+                    $Message = "ACCOUNT CREATED.";
+                } else{
+                    $Message = "could not create account. " . mysqli_error($con);
+                }
+
+      
+    }
+ else {
+    $Message = "THERE IS AN EXISTING ACCOUNT WITH THIS EMAIL";
+    
+}
+
+$response[] = array("Message" => $Message);
+
+echo json_encode($response);
+?>
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,198 +2,291 @@ import React, { Component } from 'react'
 // import './login-helper'
 import './login.css'
 import axios from 'axios';
-import {Navigate} from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 
 const API_PATH = 'http://localhost/paradox/login.php';
+const API_PATH2 = 'http://localhost/paradox/signup.php';
 
-export default class login extends Component 
-{
-  constructor(){
+export default class login extends Component {
+  constructor() {
     super();
-   
+
     this.state = {
-     email: '',
-     password: '',
-     msg:''
+      email: '',
+      password: '',
+      msg: '',
+      username: '',
+      gender: '',
+      fullName: '',
+
     };
 
     this.login = this.login.bind(this);
+    this.signup = this.signup.bind(this);
     this.onChange = this.onChange.bind(this);
 
   }
 
-  
+
 
   login() {
-    if(this.state.email && this.state.password)
-    {
+    if (this.state.email && this.state.password) {
       console.log(this.state);
       axios({
         method: 'post',
         url: `${API_PATH}`,
         headers: { 'content-type': 'application/json' },
         data: this.state
-        
+
       })
         .then(result => {
-          
-          
+
+
           // alert(result.data[0].Message);
           this.setState({
             msg: result.data[0].Message
           })
-          
-          
-           if(result.data[0].Message=='Success')
-           {
-            window.open("/dashboard","_self")
-          //   this.setState({
-          //     redirectToReferrer: true
-          //   })
-          //   console.log(this.state);
-          //   alert(result.data[0].Message);
-          //   console.log(this.state);
-          //   alert(result.data[0].Message);
-          //   console.log(this.state);
-          //   alert(result.data[0].Message);
+
+
+          if (result.data[0].Message == 'Success') {
+            window.open("/dashboard", "_self")
+            //   this.setState({
+            //     redirectToReferrer: true
+            //   })
+            //   console.log(this.state);
+            //   alert(result.data[0].Message);
+            //   console.log(this.state);
+            //   alert(result.data[0].Message);
+            //   console.log(this.state);
+            //   alert(result.data[0].Message);
 
           }
-       
+
         })
         .catch(error => this.setState({ msg: error.data[0].Message }));
-        
-     
+
+
     }
-    
-   }
 
-  onChange(e){
-    this.setState({[e.target.name]:e.target.value});
-    
-   }
-  
-  componentDidMount = () => {  
+  }
+  signup() {
+    if (this.state.email && this.state.password && this.state.fullName && this.state.username && this.state.gender) {
+      console.log(this.state);
+      axios({
+        method: 'post',
+        url: `${API_PATH2}`,
+        headers: { 'content-type': 'application/json' },
+        data: this.state
+
+      })
+        .then(result => {
+
+
+          // alert(result.data[0].Message);
+          this.setState({
+            msg: result.data[0].Message
+          })
+
+
+          if (result.data[0].Message == 'ACCOUNT CREATED.') {
+            window.open("/dashboard", "_self")
+            //   this.setState({
+            //     redirectToReferrer: true
+            //   })
+            //   console.log(this.state);
+            //   alert(result.data[0].Message);
+            //   console.log(this.state);
+            //   alert(result.data[0].Message);
+            //   console.log(this.state);
+            //   alert(result.data[0].Message);
+
+          }
+
+        })
+        .catch(error => this.setState({ msg: error.data[0].Message }));
+
+
+    }
+
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+
+  }
+
+  componentDidMount = () => {
     this.myfunction();
- }
+  }
 
- componentDidUpdate = () => {  
+  componentDidUpdate = () => {
     this.myfunction();
- }
+  }
 
- myfunction = () => {
-  const square = document.getElementById("square");
-  const frontSide = document.getElementById("frontSide");
-  const rightSide = document.getElementById("rightSide");
-  const leftSide = document.getElementById("leftSide");
+  myfunction = () => {
+    const square = document.getElementById("square");
+    const frontSide = document.getElementById("frontSide");
+    const rightSide = document.getElementById("rightSide");
+    const leftSide = document.getElementById("leftSide");
 
-  frontSide.addEventListener("click", () => {
-    frontSide.style.display = "none";
-    rightSide.style.display = "block";
-    leftSide.style.display = "block";
-    square.style.transform = "rotate3d(0, 0, 0, 90deg)";
-    document.title = "PARADOX Login";
-  });
+    frontSide.addEventListener("click", () => {
+      frontSide.style.display = "none";
+      rightSide.style.display = "block";
+      leftSide.style.display = "block";
+      square.style.transform = "rotate3d(0, 0, 0, 90deg)";
+      document.title = "PARADOX Login";
+    });
 
-  rightSide.addEventListener("click", () => {
-    rightSide.style.display = "none";
-    frontSide.style.display = "block";
-    leftSide.style.display = "block";
-    square.style.transform = "rotate3d(0, -1, 0, 90deg)";
-    document.title = "PARADOX Sign up";
-  });
+    rightSide.addEventListener("click", () => {
+      rightSide.style.display = "none";
+      frontSide.style.display = "block";
+      leftSide.style.display = "block";
+      square.style.transform = "rotate3d(0, -1, 0, 90deg)";
+      document.title = "PARADOX Sign up";
+    });
 
-  leftSide.addEventListener("click", () => {
-    leftSide.style.display = "none";
-    frontSide.style.display = "block";
-    rightSide.style.display = "none";
-    square.style.transform = "rotate3d(0, 1, 0, 90deg)";
-    document.title = "PARADOX Forgot password";
-  });
+    leftSide.addEventListener("click", () => {
+      leftSide.style.display = "none";
+      frontSide.style.display = "block";
+      rightSide.style.display = "none";
+      square.style.transform = "rotate3d(0, 1, 0, 90deg)";
+      document.title = "PARADOX Forgot password";
+    });
 
-    
- }
+
+  }
   render() {
-   
+
     return (
       <div className="login">
-      <div className="container">
-        <div id="square">
-          <div className="front">
-            <h1>Login</h1>
-            <form>
-              <input
-                required
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email"
-                onChange={this.onChange}
-              />
-              <input
-                required
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Password"
-                onChange={this.onChange}
-              />
-              <button type="button" onClick={this.login}>Submit</button>
-            </form>
-            <div>
-  {this.state.msg &&
-    <div>{this.state.msg}</div>
-  }
-</div>
+        <div className="container">
+          <div id="square">
+            <div className="front">
+              <h1>Login</h1>
+              <form>
+                <input
+                  required
+
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  onChange={this.onChange}
+                /><br />
+                <input
+                  required
+
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  onChange={this.onChange}
+                /><br />
+                <button type="button" onClick={this.login}>Submit</button>
+              </form>
+              <div>
+                {this.state.msg &&
+                  <div>{this.state.msg}</div>
+                }
+              </div>
+            </div>
+            <div className="right">
+              <h1>Create a new account</h1>
+              <form >
+                <input
+                  required
+
+                  type="email"
+                  name="email"
+                  id="email"
+
+                  placeholder="Email"
+                  onChange={this.onChange}
+                /><br />
+                <input
+                  required
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="User name"
+                  onChange={this.onChange}
+                /><br />
+                <input
+                  required
+                  type="text"
+                  name="fullName"
+                  id="fullName"
+                  placeholder="Full name"
+                  onChange={this.onChange}
+                /><br />
+                <input
+                  required
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  onChange={this.onChange}
+                /><br />
+                GENDER:
+                Male
+                <input style={{
+                  float: "left",
+                  clear: "none",
+                  margin: "2px 0 0 2px"
+                }}
+
+                  type="radio"
+                  name="gender"
+                  id="gender"
+                  value="m"
+                  placeholder="gender"
+                  onChange={this.onChange}
+                />
+                Female
+                <input
+                  style={{
+                    float: "left",
+                    clear: "none",
+                    margin: "2px 0 0 2px"
+                  }}
+                  type="radio"
+                  name="gender"
+                  id="gender"
+                  value="f"
+                  placeholder="gender"
+                  onChange={this.onChange}
+                />
+                <br /><br />
+                <button type="button" onClick={this.signup}>Submit</button>
+                <div>
+                  {this.state.msg &&
+                    <div>{this.state.msg}</div>
+                  }
+                </div>
+
+              </form>
+            </div>
+            <div className="left">
+              <h1>Add your email to reset password</h1>
+              <form action="#" method="post">
+                <input
+                  required
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                />
+                <button type="button">Submit</button>
+              </form>
+            </div>
           </div>
-          <div className="right">
-            <h1>Create a new account</h1>
-            <form action="#" method="post">
-              <input
-                required
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email"
-              />
-              <input
-                required
-                type="text"
-                name="fullName"
-                id="fullName"
-                placeholder="Full name"
-              />
-              <input
-                required
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Password"
-              />
-              <button type="submit">Submit</button>
-              
-            </form>
-          </div>
-          <div className="left">
-            <h1>Add your email to reset password</h1>
-            <form action="#" method="post">
-              <input
-                required
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email"
-              />
-              <button type="submit">Submit</button>
-            </form>
+          <div className="actions">
+            <button id="leftSide" type="button">Forgot Password</button>
+            <button id="frontSide" type="button">Login</button>
+            <button id="rightSide" type="button">Sign up</button>
           </div>
         </div>
-        <div className="actions">
-          <button id="leftSide" type="button">Forgot Password</button>
-          <button id="frontSide" type="button">Login</button>
-          <button id="rightSide" type="button">Sign up</button>
-        </div>
-      </div>
       </div>
     );
   }
