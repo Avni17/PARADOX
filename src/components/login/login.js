eab573de227e3b895344feb9eba54aedd5d45884
Avni17 <avni.arora2002@gmail.com>
2021-12-15 10:44:13 +0530

    login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -89,7 +89,7 @@ export default class login extends Component {
             msg: result.data[0].Message
           })
 
-          
+
           if (result.data[0].Message == 'ACCOUNT CREATED.') {
             localStorage.setItem('email', JSON.stringify(this.state.email));
             window.open("/dashboard", "_self")
@@ -161,136 +161,164 @@ export default class login extends Component {
   render() {
 
     return (
-      <div className="login">
-        <div className="container">
-          <div id="square">
-            <div className="front">
-              <h1>Login</h1>
-              <form>
-                <input
-                  required
+      <div className="background">
 
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-                  onChange={this.onChange}
-                /><br />
-                <input
-                  required
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
+        {<span></span>}
 
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Password"
-                  onChange={this.onChange}
-                /><br />
-                <button type="button" onClick={this.login}>Submit</button>
-              </form>
-              <div>
-                {this.state.msg &&
-                  <div>{this.state.msg}</div>
-                }
-              </div>
-            </div>
-            <div className="right">
-              <h1>Create a new account</h1>
-              <form >
-                <input
-                  required
 
-                  type="email"
-                  name="email"
-                  id="email"
+        <div className="logn">
+          <div className="container">
+            <div id="square">
+              <div className="front">
+                <h1>Login</h1>
+                <form>
+                  <input
+                    required
 
-                  placeholder="Email"
-                  onChange={this.onChange}
-                /><br />
-                <input
-                  required
-                  type="text"
-                  name="username"
-                  id="username"
-                  placeholder="User name"
-                  onChange={this.onChange}
-                /><br />
-                <input
-                  required
-                  type="text"
-                  name="fullName"
-                  id="fullName"
-                  placeholder="Full name"
-                  onChange={this.onChange}
-                /><br />
-                <input
-                  required
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Password"
-                  onChange={this.onChange}
-                /><br />
-                GENDER:
-                Male
-                <input style={{
-                  // float: "left",
-                  // clear: "none",
-                  // margin: "2px 0 0 2px"
-                }}
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    onChange={this.onChange}
+                  /><br />
+                  <input
+                    required
 
-                  type="radio"
-                  name="gender"
-                  id="gender"
-                  value="m"
-                  placeholder="gender"
-                  onChange={this.onChange}
-                />
-              
-                Female
-                <input
-                  style={{
-                    // float: "left",
-                    // clear: "none",
-                    // margin: "0px"
-                  }}
-                  type="radio"
-                  name="gender"
-                  id="gender"
-                  value="f"
-                  placeholder="gender"
-                  onChange={this.onChange}
-                />
-                <br /><br />
-                <button type="button" onClick={this.signup}>Submit</button>
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    onChange={this.onChange}
+                  /><br />
+                  <button type="button" onClick={this.login}>Submit</button>
+                </form>
                 <div>
                   {this.state.msg &&
                     <div>{this.state.msg}</div>
                   }
                 </div>
+              </div>
+              <div className="right">
+                <h1>Create a new account</h1>
+                <form >
+                  <input
+                    required
 
-              </form>
+                    type="email"
+                    name="email"
+                    id="email"
+
+                    placeholder="Email"
+                    onChange={this.onChange}
+                  /><br />
+                  <input
+                    required
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="User name"
+                    onChange={this.onChange}
+                  /><br />
+                  <input
+                    required
+                    type="text"
+                    name="fullName"
+                    id="fullName"
+                    placeholder="Full name"
+                    onChange={this.onChange}
+                  /><br />
+                  <input
+                    required
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    onChange={this.onChange}
+                  /><br />
+                  GENDER:
+                  Male
+                  <input style={{
+                    // float: "left",
+                    // clear: "none",
+                    // margin: "2px 0 0 2px"
+                  }}
+
+                    type="radio"
+                    name="gender"
+                    id="gender"
+                    value="m"
+                    placeholder="gender"
+                    onChange={this.onChange}
+                  />
+
+                  Female
+                  <input
+                    style={{
+                      // float: "left",
+                      // clear: "none",
+                      // margin: "0px"
+                    }}
+                    type="radio"
+                    name="gender"
+                    id="gender"
+                    value="f"
+                    placeholder="gender"
+                    onChange={this.onChange}
+                  />
+                  <br /><br />
+                  <button type="button" onClick={this.signup}>Submit</button>
+                  <div>
+                    {this.state.msg &&
+                      <div>{this.state.msg}</div>
+                    }
+                  </div>
+
+                </form>
+              </div>
+              <div className="left">
+                <h1>Add your email to reset password</h1>
+                <form action="#" method="post">
+                  <input
+                    required
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                  />
+                  <button type="button">Submit</button>
+                </form>
+              </div>
             </div>
-            <div className="left">
-              <h1>Add your email to reset password</h1>
-              <form action="#" method="post">
-                <input
-                  required
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-                />
-                <button type="button">Submit</button>
-              </form>
+            <div className="actions">
+
+              <button id="leftSide" type="button" >Forgot Password</button>
+              <button id="frontSide" type="button">Login</button>
+              <button id="rightSide" type="button">Sign up</button>
             </div>
-          </div>
-          <div className="actions">
-            <button id="leftSide" type="button">Forgot Password</button>
-            <button id="frontSide" type="button">Login</button>
-            <button id="rightSide" type="button">Sign up</button>
           </div>
         </div>
-      </div>
+      </div >
     );
   }
 }
