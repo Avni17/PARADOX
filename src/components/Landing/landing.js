27bdc12b42473808f928d83c38f6f48d71064b1c
Avni17 <avni.arora2002@gmail.com>
2022-07-01 14:19:44 +0530

    scroll bar

--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -1,68 +1,34 @@
 import React, { Component } from 'react';
 import './style.scss';
+import $ from 'jquery';
 
 // import signup from './images/signup.jpeg';
 
 export default class landing extends Component {
-    // myfunction = () => {
-    //     var slides = document.querySelectorAll('.slide');
-    //     var btns = document.querySelectorAll('.btn');
-    //     let currentSlide = 1;
+    myfunction = () => {
+        $(function() {
+            $(document).scroll(function() {
+              var $nav = $(".fixed-top");
+              //add background to nav when scrolled
+              $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+          
+              // $(".heading").css("top", Math.max(180 - 0.2 * window.scrollY, 0) + "px");
+              // $(".parallax1").css("opacity", Math.max(1 - 0.004 * window.scrollY, 0));
+            });
+          });
+          
+          // $(document).on('scroll', function () {
+          //     // $(".heading").css("top", Math.max(180 - 0.2 * window.scrollY, 0) + "px");
+          //     $(".parallax1").css("opacity", Math.max(1 - 0.004 * window.scrollY, 0));
+          // })
+    }
+    componentDidMount = () => {
+        this.myfunction();
+    }
 
-    //     // Javascript for image slider manual navigation
-    //     var manualNav = function (manual) {
-    //         slides.forEach((slide) => {
-    //             slide.classList.remove('active');
-
-    //             btns.forEach((btn) => {
-    //                 btn.classList.remove('active');
-    //             });
-    //         });
-
-    //         slides[manual].classList.add('active');
-    //         btns[manual].classList.add('active');
-    //     }
-
-    //     btns.forEach((btn, i) => {
-    //         btn.addEventListener("click", () => {
-    //             manualNav(i);
-    //             currentSlide = i;
-    //         });
-    //     });
-    //     var repeat = function (activeClass) {
-    //         let active = document.getElementsByClassName('active');
-    //         let i = 1;
-
-    //         var repeater = () => {
-    //             setTimeout(function () {
-    //                 [...active].forEach((activeSlide) => {
-    //                     activeSlide.classList.remove('active');
-    //                 });
-
-    //                 slides[i].classList.add('active');
-    //                 btns[i].classList.add('active');
-    //                 i++;
-
-    //                 if (slides.length == i) {
-    //                     i = 0;
-    //                 }
-    //                 if (i >= slides.length) {
-    //                     return;
-    //                 }
-    //                 repeater();
-    //             }, 3000);
-    //         }
-    //         repeater();
-    //     }
-    //     repeat();
-    // }
-    // componentDidMount = () => {
-    //     this.myfunction();
-    // }
-
-    // componentDidUpdate = () => {
-    //     // this.myfunction();
-    // }
+    componentDidUpdate = () => {
+        this.myfunction();
+    }
     render() {
         return (
             <div className='landing app'>
