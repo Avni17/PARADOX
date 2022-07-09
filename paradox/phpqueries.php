da88ad7cc703e0a15b49f867192cdb4bf08a1b98
Avni17 <avni.arora2002@gmail.com>
2022-07-09 13:38:11 +0530

    aa

--- paradox/phpqueries.php
+++ paradox/phpqueries.php
@@ -6,6 +6,17 @@ header('Access-Control-Allow-Origin: *');
     header("Content-type:application/json");
 $encodedData = file_get_contents('php://input'); 
 $decodedData = json_decode($encodedData, true);
+
+
+$UserEmail = $decodedData['email'];
+$SQL = "SELECT id FROM employee WHERE email = '$UserEmail'";
+$exeSQL = mysqli_query($con, $SQL);
+$checkid =  mysqli_num_rows($exeSQL);
+if ($checkid != 0) 
+{
+$arrayu = mysqli_fetch_array($exeSQL);
+    $id=$arrayu['id'];
+}
 //date_default_timezone_set('Asia/Kolkata'); 
 //echo date("Y-m-d H:i:s"); 
 $response[]=array();
@@ -20,7 +31,7 @@ $upcoming = "select * from project where startTime>='$todaydate'";
 $result2 = $con -> query($upcoming);
 $rowcount2=mysqli_num_rows($result2);
 
-$total = "select * from project";
+$total = "select * from team where eid=$id";
 $result3 = $con -> query($total);
 $rowcount3=mysqli_num_rows($result3);
 
