2c4e889ce0ecf49d94b267c49bc9b5a9a1687b71
Avni17 <avni.arora2002@gmail.com>
2022-07-10 13:29:11 +0530

    project total count fix

--- paradox/phpqueries.php
+++ paradox/phpqueries.php
@@ -6,27 +6,33 @@ header('Access-Control-Allow-Origin: *');
     header("Content-type:application/json");
 $encodedData = file_get_contents('php://input'); 
 $decodedData = json_decode($encodedData, true);
-//date_default_timezone_set('Asia/Kolkata'); 
-//echo date("Y-m-d H:i:s"); 
+$UserEmail = $decodedData['email'];
+$SQL = "SELECT id FROM employee WHERE email = '$UserEmail'";
+$exeSQL = mysqli_query($con, $SQL);
+$checkid =  mysqli_num_rows($exeSQL);
 $response[]=array();
-
 $todaydate = date('Y-m-d');
-
-$progress = "select * from project where startTime<='$todaydate' and endTime>='$todaydate'";
-$result1 = $con -> query($progress);
-$rowcount1=mysqli_num_rows($result1);
-
-$upcoming = "select * from project where startTime>='$todaydate'";
-$result2 = $con -> query($upcoming);
-$rowcount2=mysqli_num_rows($result2);
-
-$total = "select * from project";
+if ($checkid != 0) 
+{
+$arrayu = mysqli_fetch_array($exeSQL);
+$id=$arrayu['id'];
+$total = "SELECT DISTINCT t.pid as pid  FROM task t,project p WHERE t.pid=p.pid and eid = '$id'";
 $result3 = $con -> query($total);
 $rowcount3=mysqli_num_rows($result3);
+$progress = "SELECT DISTINCT t.pid as pid  FROM task t,project p WHERE t.pid=p.pid and eid = '$id' and startTime<='$todaydate' and endTime>='$todaydate'";
+$result1 = $con -> query($progress);
+$rowcount1=mysqli_num_rows($result1);
+$upcoming = "SELECT DISTINCT t.pid as pid  FROM task t,project p WHERE t.pid=p.pid and eid = '$id' and startTime>='$todaydate'";
+$result2 = $con -> query($upcoming);
+$rowcount2=mysqli_num_rows($result2);
+// $total = "select * from project";
+// $result3 = $con -> query($total);
+// $rowcount3=mysqli_num_rows($result3);
 
 $response[]=array("inprogress"=>$rowcount1);
 $response[]=array("upcoming"=>$rowcount2);
 $response[]=array("totalprojects"=>$rowcount3);
+}
 array_shift($response);
 
 echo json_encode($response);
