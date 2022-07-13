25f46ec4ce2442a2d5e70e8e84a0b42b144789ab
Avni17 <avni.arora2002@gmail.com>
2022-07-13 15:15:59 +0530

    navbar added in calender

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -351,12 +351,12 @@ export default class Chart extends React.Component {
         return (
             <div className='chart'>
 <div class="app-sidebar side">
-            <a href="/dashboard" class="app-sidebar-link active">
+            <a href="/dashboard" class="app-sidebar-link">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
                 <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
                 <polyline points="9 22 9 12 15 12 15 22" /></svg>
             </a>
-            <a href="/chart" class="app-sidebar-link">
+            <a href="/chart" class="app-sidebar-link active">
               <svg class="link-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="feather feather-pie-chart" viewBox="0 0 24 24">
                 <defs />
                 <path d="M21.21 15.89A10 10 0 118 2.83M22 12A10 10 0 0012 2v10z" />
